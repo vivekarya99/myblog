adb26765882aa0c860637526f79fe50ec4c6b96c
meet_cric <mp32445@gmai.com>
2018-10-22 04:13:17 +0530

    adding content to experience page

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,14 +4,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestComponent } from './interest/interest.component';
 
 const appRoutes=[
-   {path:'**',component:AboutComponent}
+   {path:'',component:AboutComponent},
+   {path:'experience',component:ExperienceComponent},
+   {path:'education',component:EducationComponent},
+   {path:'skills',component:SkillsComponent},
+   {path:'interest',component:InterestComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestComponent
   ],
   imports: [
     BrowserModule,
