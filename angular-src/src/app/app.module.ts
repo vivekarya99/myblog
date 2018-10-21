4ced1336a396a728a000210e8a9d03067da70f19
meet_cric <mp32445@gmai.com>
2018-10-22 03:18:55 +0530

    added css and jsdependency

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const appRoutes=[
+
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
