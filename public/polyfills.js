20c58271b75bb5b7d704b2f0034eb9ffea335123
meetcric <mp32445@gmail.com>
2018-10-24 01:29:03 +0530

    changing package.json for deployment to heroku v1.3

--- public/polyfills.js
+++ public/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/meet-cric/Desktop/WorkSpace/myblog/angular-src/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/meet-cric/Desktop/myblog/angular-src/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- public/styles.js
+++ public/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/meet-cric/Desktop/WorkSpace/myblog/angular-src/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/meet-cric/Desktop/myblog/angular-src/src/styles.css */"./src/styles.css");
 
 
 /***/ })
